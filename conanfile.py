6e34545e657e94727426a608c56fbce72b90905f
Kevin A. Mitchell <kam@datalogics.com>
2019-12-16 10:53:13 -0600

    Get sources
    - Use conandata.yml for the source URL and the hash code.
    
    - Remove the version number. Version number will come from how it is stored in
      the repository.
    
    - To develop locally, use a version number, username, and channel, like
      'conan install . 1.5.0@kam/testing'

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,9 @@
+import os
 from conans import ConanFile, CMake, tools
 
 
 class QrcodegeneratorConan(ConanFile):
     name = "qr_code_generator"
-    version = "1.5.0"
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
     url = "<Package recipe repository url here, for issues about the package>"
@@ -15,14 +15,11 @@ class QrcodegeneratorConan(ConanFile):
     generators = "cmake"
 
     def source(self):
-        self.run("git clone https://github.com/conan-io/hello.git")
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(HelloWorld)",
-                              '''PROJECT(HelloWorld)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+        tools.get(**self.conan_data["sources"][self.version])
+        url = self.conan_data["sources"][self.version]["url"]
+        archive_name = os.path.basename(os.path.dirname(os.path.dirname(url))) + '-' + self.version
+        os.rename(archive_name, self._source_subfolder)
+
 
     def build(self):
         cmake = CMake(self)
